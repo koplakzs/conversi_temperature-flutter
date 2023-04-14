90ee0c1e37e64fb37a8f8e54010292894a53d5d9
koplakzs <atom.go.76@gmail.com>
2023-04-15 00:17:02 +0700

    conversi temperature

---
+++ lib/screen/conversi.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Conversi extends StatefulWidget {
+  const Conversi({super.key});
+
+  @override
+  State<Conversi> createState() => _ConversiState();
+}
+
+class _ConversiState extends State<Conversi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

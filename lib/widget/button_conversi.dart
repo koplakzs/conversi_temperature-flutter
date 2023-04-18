c4809fa4803347cb97a2add93534895f4e03d986
koplakzs <atom.go.76@gmail.com>
2023-04-18 16:14:44 +0700

    improve code

--- lib/widget/button_conversi.dart
+++ lib/widget/button_conversi.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:temperature_conversi/widget/my_theme.dart';
 
 class ButtonConversi extends StatelessWidget {
